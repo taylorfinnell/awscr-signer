6daaf64cb5704387c84e151bee58db5ca5d574ec
Taylor Finnell <tmfinnell@gmail.com>
2016-12-21 07:52:23 -0500

    improve uri (#2)

--- src/awscr-signer/uri.cr
+++ src/awscr-signer/uri.cr
@@ -9,18 +9,27 @@ module Awscr
     # uri.to_s
     # ```
     class Uri
-      def initialize(uri : URI)
-        @uri = uri
+      def self.encode(path : String)
+        URI.escape(path) { |byte| URI.unreserved?(byte) || byte.chr == '/' }
       end
 
-      # Returns the uri, escaped, if needed
+      # The path must be non encoded.
+      def initialize(path : String)
+        @path = path
+      end
+
+      # The path must be non encoded.
+      def initialize(uri : URI)
+        @path = uri.path.to_s
+      end
+
+      # Returns the uri encoded
       def to_s
-        path = @uri.path.to_s
-        if path == "/"
-          "/"
-        else
-          "/" + URI.escape(path[1..-1])
-        end
+        # Allows input of /test ing/ and /test%20ing/
+        uri = URI.parse(@path)
+        path = uri.path.to_s
+        path = "/" if path.blank?
+        self.class.encode(path)
       end
     end
   end
