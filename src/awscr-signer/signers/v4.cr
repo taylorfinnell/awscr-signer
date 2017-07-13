a449d5c7c3a9d542aaea606dbf6800f31a69cc16
Taylor Finnell <tmfinnell@gmail.com>
2017-07-13 08:11:25 -0400

    dont require clients to pass signer objects

--- src/awscr-signer/signers/v4.cr
+++ src/awscr-signer/signers/v4.cr
@@ -7,13 +7,16 @@ module Awscr
       # Signs a Crystal HTTP::Request using a given scope.
       #
       # ```
-      # scope = Awscr::Signer::Scope.new("AKIDEXAMPLE", "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY",
-      #   "us-east-1", "service", Time.now)
-      # signer = Signer::V4.new(request, scope)
-      # signer.sign
+      # signer = Signer::V4.new("s3", "region", "key", "secret")
+      # signer.sign(request)
+      # signer.sign("some string")
+      # signer.presign(request)
       # ```
       class V4
-        def initialize(@scope : Scope, @credentials : Credentials)
+        def initialize(service : String, region : String, aws_access_key :
+                       String, aws_secret_key : String)
+          @scope = Signer::Scope.new(region, service)
+          @credentials = Signer::Credentials.new(aws_access_key, aws_secret_key)
         end
 
         # Sign an HTTP::Request
