fc129f3882f0cbeced1ef572c4fc73aab117e369
Taylor Finnell <tmfinnell@gmail.com>
2017-07-13 08:21:23 -0400

    update example

--- examples/request_signing.cr
+++ examples/request_signing.cr
@@ -1,27 +1,18 @@
 # Sign any Crystal HTTP::Request. Here we list S3 objects in a bucket.
 require "../src/awscr-signer"
 
-BUCKET = ENV["AWS_BUCKET"]
-HOST   = "#{BUCKET}.s3.amazonaws.com"
-KEY    = ENV["AWS_KEY"]
-SECRET = ENV["AWS_SECRET"]
-REGION = ENV["AWS_REGION"]
-
-def credentials
-  Awscr::Signer::Credentials.new(KEY, SECRET)
-end
-
-def scope
-  Awscr::Signer::Scope.new(REGION, "s3")
-end
+SERVICE = "s3"
+BUCKET  = ENV["AWS_BUCKET"]
+KEY     = ENV["AWS_KEY"]
+SECRET  = ENV["AWS_SECRET"]
+REGION  = ENV["AWS_REGION"]
+HOST    = "#{BUCKET}.#{SERVICE}.amazonaws.com"
 
 def client(host, &block)
   client = HTTP::Client.new(host)
 
   client.before_request do |request|
-    request.headers["Host"] = HOST
-
-    signer = Awscr::Signer::V4.new(scope, credentials)
+    signer = Awscr::Signer::V4.new(SERVICE, REGION, KEY, SECRET)
     signer.sign(request)
   end
 
