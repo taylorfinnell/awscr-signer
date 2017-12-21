9d2d008864c1fbd61995582425afeccbb0b96e0b
Taylor Finnell <tmfinnell@gmail.com>
2017-12-21 17:34:09 -0500

    remove signers.cr

--- src/awscr-signer.cr
+++ src/awscr-signer.cr
@@ -1,6 +1,7 @@
 require "./awscr-signer/core/*"
 require "./awscr-signer/v4/*"
-require "./awscr-signer/*"
+require "./awscr-signer/v2/*"
+require "./awscr-signer/signers/*"
 
 module Awscr
   # Implementation of AWS request signing
